8e420d0b02200cdf539c75dbaef17d833068833c
VaddiBhargavi <bhargavivaddi999@gmail.com>
2019-07-31 12:38:12 +0530

    initial commit

---
+++ src/main/java/com/stackroute/centralisedconfigservice/CentralisedconfigserviceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.centralisedconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralisedconfigserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralisedconfigserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stackroute/centralisedconfigservice/CentralisedconfigserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stackroute.centralisedconfigservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CentralisedconfigserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
